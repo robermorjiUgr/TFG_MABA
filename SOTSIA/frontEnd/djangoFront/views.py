32bc12b29e5ebbc3559c1c6fdfb234c733676ba4
Miguel Benítez <miguealguacil@gmail.com>
2022-01-18 17:19:07 +0100

    Add registration templates

--- SOTSIA/frontEnd/djangoFront/views.py
+++ SOTSIA/frontEnd/djangoFront/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'sotsia/home.html')
+    return render(request, 'home.html')
 
 def dashboard(request):
     return render(request, 'sotsia/dash.html')
--- SOTSIA/frontEnd/djangoSOTSIA/settings.py
+++ SOTSIA/frontEnd/djangoSOTSIA/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #own
-    'djangoFront.apps.DjangofrontConfig',
+    'djangoFront',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'djangoSOTSIA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'djangoFront' / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,6 +71,10 @@ TEMPLATES = [
 ]
 
 LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
 
 WSGI_APPLICATION = 'djangoSOTSIA.wsgi.application'
 
