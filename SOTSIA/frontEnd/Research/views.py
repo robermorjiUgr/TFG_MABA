0f99cf00d9cfe3c91b5e3ed35ac2ba6dd0a8bf79
Miguel Benítez <miguealguacil@gmail.com>
2022-02-01 18:37:59 +0100

    Create Dataset page and charts added in Testing

--- SOTSIA/frontEnd/Research/views.py
+++ SOTSIA/frontEnd/Research/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
 
-import requests
-
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def testing(request):
-    url = 'http://localhost:5000/dataset/dbName'
-    res = requests.request(method="GET", url=url)
-    list_dict = res.json()
-    list_dbName = []
-    for dict in list_dict:
-        for key,value in dict.items():
-            list_dbName.append(value) 
+    # url = 'http://localhost:5000/dataset/dbName'
+    # res = requests.request(method="GET", url=url)
+    # list_dict = res.json()
+    # list_dbName = []
+    # for dict in list_dict:
+    #     for key,value in dict.items():
+    #         list_dbName.append(value) 
 
-    url = 'http://localhost:5000/dataset/data'
-    res = requests.request(method="GET", url=url)
-    data_db = res.json()
+    # url = 'http://localhost:5000/dataset/data'
+    # res = requests.request(method="GET", url=url)
+    # data_db = res.json()
     
 
-    if request.user.is_authenticated:
-        args = {'user_authenticated' : 'true', 'database_name':list_dbName, 'data':data_db }
-        template = 'sotsia/testing.html'
-    else:
-        args = {'user_authenticated' : 'false' }
-        template = 'sotsia/testing-fail.html'
-    return render(request, 'sotsia/testing.html', args)
+    # if request.user.is_authenticated:
+    #     args = {'user_authenticated' : 'true', 'database_name':list_dbName, 'data':data_db }
+    #     template = 'sotsia/testing.html'
+    # else:
+    #     args = {'user_authenticated' : 'false' }
+    #     template = 'sotsia/testing-fail.html'
+    # return render(request, 'sotsia/testing.html', args)
+    return render(request, 'sotsia/testing.html')
 
+def dataset(request):
+    return render(request, 'sotsia/dataset.html')
